705cd764200012484b645f27493fb79bde83e78d
Dawid-Oskwarek <dawidoskwarek97@gmail.com>
2023-01-02 15:39:11 +0000

    cypress setup

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,8 +2,10 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
 
 module.exports = (on) => {
-  on('file:preprocessor', cucumber());
-  installLogsPrinter(on, {
+  on('file:preprocessor', cucumber({
+    nonGlobalStepDefinitions: true
+  }));
+    installLogsPrinter(on, {
     printLogsToConsole: 'always',
   });
 };
